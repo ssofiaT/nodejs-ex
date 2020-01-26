e54b2ba13984bf7df4472b0aea825369fe59983a
Oleksandr Tkachenko <oleks.tkach@gmail.com>
2020-01-25 23:17:28 -0800

    Runtime error

--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -53,7 +53,7 @@ router.post('/', (req, res) => {
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
-            // console.log(webhook_event);
+            console.log(webhook_event);
 
             let sender_id = webhook_event.sender.id;
             let recipient_id = webhook_event.recipient.id;
