3e0aadbdd79fe8368e6588679134ca9121886259
ssofiaT <tkasof@gmail.com>
2020-02-10 00:19:05 -0800

    fix compilation error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const
 Object.assign = require('object-assign')
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.engine('html', require('ejs').renderFile);
 // morgan to use dev format
