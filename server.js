cc16b1c17025edcb80757ed464a03a522bf00a3e
ssofiaT <tkasof@gmail.com>
2020-02-09 23:51:12 -0800

    Added Port/IP

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const
   morgan = require('morgan'),
   bodyParser = require('body-parser'),
   webhookRoute = require('./api/routes/webhook');
-
+  
 Object.assign = require('object-assign')
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 
 app.engine('html', require('ejs').renderFile);
 // morgan to use dev format
